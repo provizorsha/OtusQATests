8dea3080b6fc9d67e14770861296152a16cdb7a4
Kate Kutsenko <ekaterina@MacBook-Pro.local>
2020-11-08 09:10:47 +0300

    Initial commit

---
+++ junit/src/test/java/SampleTest.java
@@ -0,0 +1,40 @@
+import config.ServerConfig;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class SampleTest {
+    protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(SampleTest.class);
+    private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+
+    @Test
+    public void log(){
+        logger.info("this is info");
+    }
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("Драйвер поднят");
+    }
+    @Test
+    public void openPage(){
+        driver.get(cfg.url());
+        logger.info("Открыта страница Dot-Dot");
+    }
+    @After
+    public void setDown(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+}
